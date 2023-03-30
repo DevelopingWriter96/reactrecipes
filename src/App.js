2c35ec46dd5e75d29038d094208c52d8b447c1ac
Richard Stacey <staceyrichard1996@gmail.com>
2023-03-30 14:16:35 -0600

    Beginning static data structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Recipe() {
+  return (
+    <>
+      <h1>Let's get cooking!</h1>
+    <h2>Cucco Quiche</h2>
+    <ul>
+      <li>12 Servings</li>
+      <li>40 Minutes</li>
+    <h3>Ingredients</h3>
+    <ul>
+      <li>2 Large Eggs</li>
+      <li>1/4 Cup of Milk (60 ml)</li>
+    </ul>
+    </ul>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Recipe />
+    </>
   );
 }
 
